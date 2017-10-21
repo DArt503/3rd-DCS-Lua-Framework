4aa70b5fb8fb6f6bb697420e0a636b1896a17d00
Ked <Kedthunder@gmail.com>
2017-10-22 00:48:15 +0200

    Added polygon zones creation

---
+++ modules/polygon-zone/polygon-zone.lua
@@ -0,0 +1,22 @@
+-- Polygon Zone Module
+-- Ked
+
+PolygonModule = {
+    polygonZones = {}
+}
+
+function PolygonModule:register(groupName, zoneName)
+    table.insert(PolygonModule.polygonZones, ZONE_POLYGON:New(zoneName, GROUP:FindByName(groupName))) 
+end
+
+function PolygonModule:unregister(zoneName) 
+    local i = 0
+    for _,v in pairs(PolygonModule.polygonZones) do
+        if v:GetName() == zoneName then
+            table.remove(PolygonModule.polygonZones, i)
+            do return end
+        end
+        i = i + 1
+    end
+end
+
