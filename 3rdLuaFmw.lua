c4dceb7c416fd1b3edda55f003552ad231964d30
Ked <Kedthunder@gmail.com>
2017-12-04 15:47:38 +0100

    Added air traffic module
    Added the air trafic module, it's still very basic but you can spawn and customize this completely in one line
    Todo next : A function that spawn an already created RAT group and a function to delete a RAT group

--- 3rdLuaFmw.lua
+++ 3rdLuaFmw.lua
@@ -119,3 +119,64 @@ function PolygonModule:unregister(zoneName)
     _com:uninit()
 end
 
+-- Air Traffic Module
+-- Ked
+
+AirTrafficModule = {
+    traffic = {}
+}
+
+function AirTrafficModule:addTrafic(groupName, departure, destination, coalition, commute, takeoff, minDistance, maxDistance, FLmin, FLmax, FLCruise, number)
+    _com:init()
+   local newTraffic = RAT:New(groupName)
+
+   --Name of the departure airport, nil if random, can be a zone name or a table of names/zones
+   if departure ~= nil then
+        newTraffic:SetDeparture(departure)
+   end
+   --Name of the destination airport, nil if random, can be a zone name or a table of names/zones
+   if destination ~= nil then
+        newTraffic:SetDestination(destination)
+   end
+   
+   if coalition == 1 then -- 0 -> own coalition + neutral : default | 1 -> sameonly : Own coalition only 
+        newTraffic:SetCoalition("sameonly")
+   end
+   -- If true, will do trip back to its departure airport
+   if commute == true then
+        newTraffic:Commute()
+   end
+   -- "cold", "hot" or "air" -> where you want it to start, if 
+   -- Default is 50/50 chance of cold or hot
+   if takeoff ~= nil then
+        newTraffic:SetTakeoff(takeoff)
+   end
+   
+   if minDistance > 0 then
+        newTraffic:SetMinDistance(minDistance)
+   end
+
+   if maxDistance > 0 then
+        newTraffic:SetMaxDistance(maxDistance)
+   end
+
+   if FLmin > 0 then 
+        newTraffic:SetFLmin(FLmin)
+   end
+
+   if FLmax > 0 then
+        newTraffic:SetFLmax(FLmax)
+   end
+
+   if FLCruise > 0 then
+        newTraffic:SetFLcruise(FLCruise)
+   end
+
+   newTraffic:Spawn(number)
+   table.insert(AirTrafficModule.traffic, newTraffic)     
+   _com:log( "Spawned the AirTraffic "..groupName )
+end
+
+function AirTrafficModule:deleteTraffic(zoneName) 
+   
+end
